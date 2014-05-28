fca559fd341c2ab30497c607ac810b336cce9035
TheOpenDevProject <tkgs28@gmail.com>
2014-05-28 22:45:32 +1000

    HTML Templates
    HTML Templates

---
+++ Sally/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<head>
+<title>Sally's Clothes Shop</title>
+<link href="/style/style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div id="clean-header">Sally's Clothes</div>
+	<div id="center-fix">
+	<div class="pos-system-select" id="pos-green"><div class="txt-f-r">Add Customer</div></div>
+	<div class="pos-system-select" id="pos-red"><div class="txt-f-r">Create Purchases</div></div>
+	<div class="pos-system-select" id="pos-blue"><div class="txt-f-r">Product Manager</div></div>
+	<div class="pos-system-select" id="pos-yellow"><div class="txt-f-r">Records</div></div>
+	</div>
+</body>
